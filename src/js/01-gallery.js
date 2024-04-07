29d46f801971a57e17c0c7f3727176db3226a0de
MartaAnetaSzymanska <szymanska.ma@gmail.com>
2024-04-07 23:22:30 +0200

    zadanie-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,28 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const listItems = galleryItems
+  .map(
+    galleryItem =>
+      `<li><a class="gallery__item" href="${galleryItem.original}">
+    <img class="gallery__image" src="${galleryItem.preview}" 
+    alt="${galleryItem.description}"/></a></li>`
+  )
+  .join('');
+gallery.innerHTML = listItems;
+
+gallery.addEventListener('click', event => {
+  event.preventDefault();
+  const lightbox = new SimpleLightbox(`.gallery a`, {
+    captions: true,
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+  lightbox.open();
+});
