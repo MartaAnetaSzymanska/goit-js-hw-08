cf1391d1c89f6ef0f40a891af334b1fb9ddde9b0
MartaAnetaSzymanska <szymanska.ma@gmail.com>
2024-04-08 16:48:08 +0200

    zadanie-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe#vimeo-player');
+const player = new Player(iframe);
+
+const delayedUpdate = throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000);
+
+player.on('timeupdate', function (data) {
+  delayedUpdate(data);
+});
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(Number.parseFloat(currentTime));
+}
